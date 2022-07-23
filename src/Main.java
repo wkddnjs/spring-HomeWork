f37cdebc720d225355771e7124052b1403e4e2bf
wkddnjs <wkddnjs0416@gmail.com>
2022-07-23 16:11:00 +0900

    시나리오 주석 제거

--- src/Main.java
+++ src/Main.java
@@ -5,77 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        // 택시 테스트
-        //- 기본값
-        //    - 최대 승객 수 = 4
-        //    - 기본 요금 = 3000
-        //    - 거리당 요금 = 1000
-        //    - 기본 거리 = 1
-        //    - 주유량 = 100
-        //    - 상태 = 일반
-        //    - 속도 = 0
-
-        // -->택시 객체 생성 및 기본값 테스트 완료 및 택시시나리오에 있는 몇몇 출력값 확인 완료
-
-//        Texi texi = new Texi();
-//        ArrayList<Integer> arrayList = new ArrayList<>();
-//
-//
-//        texi.setMaxPassengers(4);
-//        System.out.println(texi.getMaxPassengers());
-//        System.out.println(texi.getBasic_price());
-//        System.out.println(texi.getDistancePer_price());
-//        System.out.println(texi.getBasic_distance());
-//        System.out.println(texi.getFuelStatus());
-//        System.out.println(texi.getVelocity());
-//        texi.statusChange("일반");
-//        System.out.println(texi.getStatus());
-//
-//        texi.setNowPassengers(2);
-//        int nowTextPassen = texi.getNowPassengers();
-//        System.out.println(texi.leftPassenger(nowTextPassen));
-//
-//        int per_price0 = texi.addpricePerdistance(2);
-//        System.out.println(per_price0+"원입니다.");
-//        arrayList.add(per_price0);
-//
-//        int per_price1 = texi.addpricePerdistance(12);
-//        System.out.println(per_price1+"원입니다.");
-//        arrayList.add(per_price1);
-//
-//        System.out.println(arrayList);
-//        int sum = 0;
-//        for(int arr : arrayList){
-//
-//            sum+=arr;
-//
-//        }
-//
-//        System.out.println("누적금액은 "+sum+" 원입니다.");
-
-
         System.out.println("===========================================");
-
-        //    - 기본값
-        //    - 최대 승객 수 = 30
-        //    - 상태 = 운행
-        //    - 요금 = 1000
-
-        // -->버스 객체 생성 및  기본값 테스트 완료
-
-//        Bus bus = new Bus();
-//        System.out.println(bus.getFuelStatus());
-//        bus.statusChange("운행중");
-//        System.out.println(bus.getStatus());
-//        System.out.println(bus.getFuelStatus()+"입니다.");
-//        bus.setMaxPassengers(30);
-//        bus.setNowPassengers(2);
-//        bus.setFee(1000);
-//        int examp = bus.countingBusFee(bus.getNowPassengers());
-//        System.out.println(examp+"원");
-//        int leftPassenger =  bus.leftPassenger(bus.getNowPassengers());
-//        System.out.println(leftPassenger);
 
         // 스캐너 입력받는 함수.
         Scanner scanner = new Scanner(System.in);
@@ -233,20 +163,13 @@ public class Main {
                                                 System.out.println("1. 내린다");
                                                 System.out.println("2. 내리지 않는다");
 
-//**************************************************************************
-//**************************************************************************
-//*********************************Q7***************************************
                                                 while (true) {
                                                     int q6 = scanner.nextInt();
                                                     if (q6 == 1) {
                                                         System.out.println("아직 내리실 때가 아닙니다!조금만 더 파이팅!");
 
                                                     } else if (q6 == 2) {
-//                                                        System.out.println("다음 정류장으로 이동합니다.");
                                                         bus.setFee(5000);
-                                                        //bus.setNowPassengers(5);
-                                                        //System.out.println("탑승 승객 수: " + bus.getNowPassengers() + "  잔여 승객 수: " + (bus.leftPassenger(5))+"  요금: " + (bus.getFee()));
-//                                                        System.out.println("=========================================================");
                                                         System.out.println(" ");
                                                         System.out.println("Q7. 버스가 항해 3달차 정류장에 정차하였습니다. 번화가라서 많은 사람이 내립니다.");
                                                         System.out.println("내리시겠습니까?" );
@@ -271,14 +194,6 @@ public class Main {
 
                                                             }
                                                             else if(q7==2){
-//                                                                System.out.println("----------\n");
-//                                                                System.out.println("다음 정류장으로 이동합니다.");
-//                                                                System.out.println("버스 상태: 차고지 행"+"연료량: " + bus.fuelChanged(115));
-//                                                                System.out.println("=========================================================");
-
-
-//*********************************Q8***************************************
-//*********************************Q8***************************************
 
                                                                 System.out.println("Q8. 항해99버스가 드디어 목적지에 도착하였습니다.\n" +
                                                                         "이 버스는 당신의 열정을 연료로 굴러갔습니다!" +
@@ -305,7 +220,7 @@ public class Main {
                                                 }
                                             }
                                         }
-//**************************************************************************
+//**********************************택시 시나리오 시작 ****************************************
 
                                     }
 
@@ -499,15 +414,6 @@ public class Main {
                                                         System.out.println("상태 : 운행불가 ");
 
 
-
-
-
-                                                        //System.out.println("누적요금 : " + total_list+totals);
-
-
-//
-//                                                        System.out.println(arrayList);
-
                                                         int sum = 0;
                                                         for(int i =0; i < arrayList.size(); i++){
 
@@ -520,24 +426,8 @@ public class Main {
 
                                                         System.out.println("1. 수고하셨습니다.");
 
-
-
                                                     }
                                                 }
-
-
-
-//                                                    texi.setTrans_Num(1);
-//                                                    System.out.println("기본 요금 : " + texi.getBasic_price());
-//                                                    System.out.println("추가 요금 : " + texi.getBasic_maxprice());
-//                                                    System.out.println("현재 요금 : " + texi.getBasic_price() +texi.getBasic_maxprice() );
-//                                                    System.out.println("목적지 : " + texi.getDeparture());
-//
-//                                                    System.out.println("기존 목적지 까지 거리 : " + texi.getDeparture_distance() + " km ");
-//                                                    System.out.println("감소된 목적지 까지 거리 : "+  texi.getDeparture_mindistance() + "km" );
-
-
-
                                             }
 
                                         }
@@ -549,24 +439,11 @@ public class Main {
                     }else if(q2_taxi == 2){
 
                         System.out.println("옳바른 경로가 아닙니다.");
-
-
                     }
                 }
-
-
-
-
             } else {
-
                 System.out.println("1,2번중에 선택해주세요");
-
-
             }
-
         }
-
-
     }
-
 }
